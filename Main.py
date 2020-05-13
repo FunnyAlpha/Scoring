d98e47ac2ec4e7757e2396a51f4722f2e766835c
Maxim <blazeteamprojects@gmail.com>
2020-05-13 10:02:09 +0300

    output pool of predictors to dataframe

--- Main.py
+++ Main.py
@@ -1,5 +1,7 @@
 from Application import *
 from Predictors import *
+import numpy as np
+from tabulate import tabulate
 
 if __name__ == "__main__":
 
@@ -7,26 +9,25 @@ if __name__ == "__main__":
     vector_dwh.getCreditBureauData()
     df_input = vector_dwh.product.CreditBureau_df
 
-    #print (vector_dwh.product.CreditBureau_df)
+    #print (df_input)
 
     #print(df_input['SK_APPLICATION'].dtype)
     #print(list(df_input))
-
-    '''
-    print(     df_input.loc[
-        (df_input['SK_APPLICATION'] == 203388465) &
-        (df_input['NFLAG_CREDIT_JOINT'] == 1) &
-        (df_input['FLAG_CREDIT_OWNER'] == '0')
-        #is_card(df['CREDIT_TYPE']) == 1
-        ,'DTIME_CREDIT'
-        ].max() 
-    )
-    '''
     
    # dd = {'sk_application': [203388465], 'max_date_open_card': [None]}
    # dff = pd.DataFrame(data=dd)
    # print(dff)
 
-    predictors_dwh  = TestScoreCardPredictors()
-    predictors_dwh.get_predictors_rez_df(203388465,df_input)
-    print (predictors_dwh.Predictors_df)
+    print(
+    df_input[
+    (df_input['NFLAG_CREDIT_JOINT'] == 1) &
+    (df_input['FLAG_CREDIT_OWNER'] == '0') &
+    (df_input['CREDIT_TYPE'].apply(lambda x:1 if x in {4,14,24} else 0)) == 1
+    ].groupby(['SK_APPLICATION']).agg({'DTIME_CREDIT':np.min}).rename(columns={"DTIME_CREDIT": "MIN_DATE_OPEN_CARD"})
+    )
+
+    predictors_dwh  = TestScoreCardPredictors(df_input)
+    predictors_dwh.get_predictors_rez_df(df_input)
+
+    print(tabulate(predictors_dwh.predictors_out_df, headers='keys',tablefmt='psql',disable_numparse=True))
+    #print (predictors_dwh.predictors_out_df)
--- Predictors.py
+++ Predictors.py
@@ -1,48 +1,71 @@
 import pandas as pd
+import numpy as np
+from functools import reduce
 
 class Predictors:
     #construct object with null df
-    #def __init__(self)->None:
-    #    self.Predictors_df = None
+    def __init__(self,predictors_cb_df):
+        self.predictors_cb_df = predictors_cb_df
+        self.predictors_out_df = None
 
     def is_card(self,credit_type):
-        return 1 if credit_type in {4,14,24}  else 0
+        return 1 if credit_type == 4  else 0
     
     
-    def max_date_open_card(self,p_sk_application,df):
-        return
-        df.loc[
-        (df['SK_APPLICATION'] == p_sk_application) &
+    def max_date_open_card(self,df):
+
+        #print(df['CREDIT_TYPE'].dtype)
+        #print(df['CREDIT_TYPE'].apply(lambda x:1 if x in {4,14,24} else 0))
+
+        v_df=df[
         (df['NFLAG_CREDIT_JOINT'] == 1) &
         (df['FLAG_CREDIT_OWNER'] == '0') &
-        is_card(df['CREDIT_TYPE']) == 1
-        ,'DTIME_CREDIT'
-        ].max() 
+        (df['CREDIT_TYPE'].apply(lambda x:1 if x in {4,14,24,5} else 0)) == 1
+        ].groupby(['SK_APPLICATION']).agg({'DTIME_CREDIT':np.max}).rename(columns={"DTIME_CREDIT": "MAX_DATE_OPEN_CARD"})
+    
+       # df.rename(columns={"B": "c"})
+
+        return v_df
+    
+    def min_date_open_card(self,df):
+
+        #print(df['CREDIT_TYPE'].dtype)
+        #print(df['CREDIT_TYPE'].apply(lambda x:1 if x in {4,14,24} else 0))
+
+        v_df=df[
+        (df['NFLAG_CREDIT_JOINT'] == 1) &
+        (df['FLAG_CREDIT_OWNER'] == '0') &
+        (df['CREDIT_TYPE'].apply(lambda x:1 if x in {4,14,24} else 0)) == 1
+        ].groupby(['SK_APPLICATION']).agg({'DTIME_CREDIT':np.min}).rename(columns={"DTIME_CREDIT": "MIN_DATE_OPEN_CARD"})
+  
+
+        return v_df
+
 
 
 class TestScoreCardPredictors(Predictors):
 
-    def __init__(self)->None:
-        self.Predictors_df = None
+    def __init__(self,predictors_cb_df):
+        super().__init__(predictors_cb_df)
+        
+        
+    def get_predictors_rez_df(self,df)->None:
 
-    def get_predictors_rez_df(self,p_sk_application,df)->None:
+        #print(df['SK_APPLICATION'])
 
-        print(p_sk_application)
-        print(df)
+        #print(Predictors.max_date_open_card(self,df))
+        df_base = df['SK_APPLICATION'].unique() 
+        df1 = Predictors.max_date_open_card(self,df)
+        df2 = Predictors.min_date_open_card(self,df)
 
-        v_max_date_open_card = Predictors.max_date_open_card(self,p_sk_application,df)
+        dfs = [df.set_index(['SK_APPLICATION']) for df in [df_base,df1,df2]]
 
-        print(v_max_date_open_card)
+        #data_frames = [df_base,df1,df2]
+        #df_merged = reduce(lambda  left,right: pd.merge(left,right,how='outer',on=['SK_APPLICATION']), data_frames)
 
-        d = {
-        'sk_application':[p_sk_application],
-        'max_date_open_card':[v_max_date_open_card]
-        }
 
-        #print(d)
-
-        df = pd.DataFrame(data=d)
-
-        #print(df)
-
-        self.Predictors_df = df
+        #print(df1)
+        self.predictors_out_df = pd.concat(dfs, axis=1).reset_index()
+        #self.predictors_out_df = pd.merge(df1,df2,how='outer',on=['SK_APPLICATION'])
+        #self.predictors_out_df = pd.merge(df1,df2,how='outer',on=['SK_APPLICATION'])
+        #self.predictors_out_df = df1.set_index('SK_APPLICATION').join(df2.set_index('SK_APPLICATION'))
